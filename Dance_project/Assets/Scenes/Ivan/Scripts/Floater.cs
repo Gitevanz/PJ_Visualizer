9eb805630e9c55c1d98ca611c1ef9e1ed9ef606a
Krakotun <evan.zayats@gmail.com>
2022-12-11 23:15:15 +0200

    Slime added

---
+++ Dance_project/Assets/Scenes/Ivan/Scripts/Floater.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+// Makes objects float up & down while gently spinning.
+public class Floater : MonoBehaviour
+{
+    // User Inputs
+    public float degreesPerSecond = 15.0f;
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    // Use this for initialization
+    void Start()
+    {
+        // Store the starting position & rotation of the object
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Spin object around Y-Axis
+        float floatrotation = Mathf.Sin(Time.fixedTime * Mathf.PI) * degreesPerSecond;
+
+transform.Rotate(new Vector3(0f, floatrotation, 0f), Space.World);
+
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.x += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+}
